7c9cf5386dcee91a399f1afc3751698e1d168b01
Костянтин Поліщук <kostiantyn.polishchuk@ria.com>
2021-10-22 16:27:14 +0300

    initial

---
+++ fibonacci.js
@@ -0,0 +1,7 @@
+/**
+* @param {ingeger} input  valut to calculate the Fibonacci number
+* @returns {number} value of max fibonaccis number 
+*/
+module.exports = (input) => {
+ // myAwsesomeAlgorithm
+}   
