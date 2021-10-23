98ee1c53a404e6fa35bb33bbd37ebe74615f9092
4r1em <art.beloshapka@gmail.com>
2021-10-23 10:01:58 +0300

    Finished fibonacci function

--- fibonacci.js
+++ fibonacci.js
@@ -3,5 +3,21 @@
 * @returns {number} value of max fibonaccis number 
 */
 module.exports = (input) => {
- // myAwsesomeAlgorithm
-}   
+    // myAwsesomeAlgorithm
+    if (!Number.isInteger(input)) throw new Error('Number must be int');
+    if (input < 0) throw new Error('Number must be positive')
+    if (input <= 1) return input
+
+    let start = 0
+    let point = 1
+    let end
+
+    for (let i = 1; i < input; i++) {
+        end = start + point
+        start = point
+        point = end
+
+    }
+    return point
+}
+
